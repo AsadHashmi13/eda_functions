4a493cfb6cf2d825634b9da196b5a70bb2df6037
Asad Ullah Hashmi <asaduhashmi@gmail.com>
2025-12-08 18:21:11 +0500

    Added missing column percentage function

--- eda_py_helper.py
+++ eda_py_helper.py
@@ -7,3 +7,7 @@ def detailed_value_counts(df,col):
     results_df = pd.DataFrame({'Count': counts, 'Proportion': proportions})
     results_df.reset_index(inplace=True)
     return results_df
+
+def missing_value_percentage(df):
+    percent_missing = df.isnull().sum() * 100 / len(df)
+    return pd.DataFrame({'percent_missing': percent_missing})
